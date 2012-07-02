d79b0230f379943581b72724145d6729bc96e3cc
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-02 18:10:10 +0200

    Converted actor-listener to use the macro

--- src/okku/samples/pi/okku.clj
+++ src/okku/samples/pi/okku.clj
@@ -44,23 +44,18 @@
                                        (- (System/currentTimeMillis) start)))
                (-> this .getContext (.stop self))))])
 
-(defn actor-listener [context name]
-  (.actorOf context
-            (Props. (proxy [UntypedActorFactory] []
-                      (create []
-                        (proxy [UntypedActor] []
-                          (onReceive [msg] (condp = (:type msg)
-                                             :pi-approximation (do
-                                                                 (println (format "\n\tPi approximation: \t\t%1.8f\n\tCalculation time: \t%8d millis"
-                                                                                  (:pi msg) (:duration msg)))
-                                                                 (-> this .getContext .system .shutdown))
-                                             (.unhandled this msg)))))))))
+(defactory actor-listener [self sender {t :type pi :pi dur :duration}]
+  [:dispatch-on t
+   :pi-approximation (do
+                       (println (format "\n\tPi approximation: \t\t%1.8f\n\tCalculation time: \t%8d millis"
+                                        pi dur))
+                       (-> this .getContext .system .shutdown))])
 
 (defn -main [& args]
   (let [nw (if args (Integer/parseInt (first args)) 4)
         ne 10000 nm 10000
         sys (ActorSystem/create "PiSystem")
-        listener (actor-listener sys "listener")
+        listener (actor-listener :context sys :name "listener")
         master (actor-master nw nm ne listener :context sys :name "master")]
     (println "Number of workers: " nw)
     (.tell master (message-compute))
