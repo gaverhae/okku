b72e16972f69bd88cf4e3e736c40ae614b9f579e
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-22 17:47:14 +0200

    Refactored in smaller functions, added some documentation and some tests

--- src/okku/core.clj
+++ src/okku/core.clj
@@ -6,34 +6,47 @@
            [com.typesafe.config ConfigFactory])
   (:require [clojure.walk :as w]))
 
-(defn round-robin-router [n]
+(defn round-robin-router
   "Creates a round-robin router with n replicas."
-  (RoundRobinRouter. n))
+  [n] (RoundRobinRouter. n))
 
-(defn actor-system [name & {:keys [config file port local hostname]
-                            :or {file "application"
-                                 config false
-                                 port 2552
-                                 hostname "127.0.0.1"
-                                 local false}}]
+(defn- base-remote-config [port hostname]
+  (ConfigFactory/parseString
+    (format "akka.remote.netty.port = %d
+            akka.remote.netty.hostname = \"%s\"
+            akka.actor.provider = akka.remote.RemoteActorRefProvider"
+            port hostname)))
+
+(defn- restrict-config [config-object path]
+  (if path
+    (.getConfig config-object path)
+    config-object))
+
+(defn- remote-config [config-object local? port hostname]
+  (if-not local?
+    (.withFallback config-object (base-remote-config port hostname))
+    config-object))
+
+(defn actor-system
   "Creates a new actor system.
-  config should be the name of the corresponding section in the application.conf file.
-  file should be the name of the config file (.conf appended by the library).
-  port should be the port number for this ActorSystem (lower priority than config file).
-  local creates a local actor system (port option is then ignored, default to false)."
+  name is used in the path to any actor in this system.
+  :config should be the name of the corresponding section in the config file.
+  :file should be the name of the config file (.conf appended by the library).
+  :port should be the port number for this ActorSystem (lower priority than config file).
+  :hostname should be the hostname for this ActorSystem (lower priority than config file).
+  :local creates a local actor system (port and hostname options are then ignored; default to false)."
+  [name & {:keys [config file port local hostname]
+           :or {file "application"
+                config false
+                port 2552
+                hostname "127.0.0.1"
+                local false}}]
   (ActorSystem/create
     name
     (ConfigFactory/load
       (-> (ConfigFactory/parseResourcesAnySyntax file)
-        (#(if config (.getConfig % config) %))
-        (#(if-not local
-            (.withFallback %
-              (ConfigFactory/parseString
-                (format "akka.remote.netty.port = %d
-                        akka.remote.netty.hostname = \"%s\"
-                        akka.actor.provider = akka.remote.RemoteActorRefProvider"
-                        port hostname)))
-            %))))))
+        (restrict-config config)
+        (remote-config local port hostname)))))
 
 (defmacro !
   "Sends the msg value as a message to target, or to current sender if target
@@ -41,44 +54,92 @@
   ([msg] `(.tell (.getSender ~'this) ~msg (.getSelf ~'this)))
   ([target msg] `(.tell ~target ~msg (.getSelf ~'this))))
 
-(defmacro dispatch-on [dv & forms]
+(defmacro dispatch-on
+  "Bascially expands to a cond with an equality test on the dispatch value dv,
+  then adds the final :else form to call the .unhandled method on self for
+  compatibility with Akka expectations."
+  [dv & forms]
   `(cond ~@(mapcat (fn [[v f]] `[(= ~dv ~v) ~f]) (partition 2 forms))
          :else (.unhandled ~'this ~dv)))
 
-(defmacro spawn [act & {c :in r :router n :name d :deploy-on}]
-  (let [c (if c c '(.getContext this))
-        p (-> act
-            (#(if r `(.withRouter ~% ~r) %))
-            (#(if d `(.withDeploy ~% (Deploy. (RemoteScope. (cond (instance? String ~d)
-                                                                  (AddressFromURIString/parse ~d)
-                                                                  (sequential? ~d)
-                                                                  (condp = (count ~d)
-                                                                    3 (Address. "akka"
-                                                                                (nth ~d 0)
-                                                                                (nth ~d 1)
-                                                                                (nth ~d 2))
-                                                                    4 (Address. (nth ~d 0)
-                                                                                (nth ~d 1)
-                                                                                (nth ~d 2)
-                                                                                (nth ~d 3))
-                                                                    (throw (IllegalArgumentException. "spawn:deploy-on should be either a String or a sequence of 3 or 4 elements")))
-                                                                  :else (throw (IllegalArgumentException. "spawn:deploy-on should be either a String or a sequence of 3 or 4 elements")))))) %)))]
+(defn- with-router [actor-spec r]
+  (if r
+    `(.withRouter ~actor-spec ~r)
+    actor-spec))
+
+(defn parse-address
+  "Returns an akka.actor.Address from either a string representing the address
+  or a four (or three) element sequence containing the four parts of an
+  address: the protocol (defaults to \"akka\" if it's a 3 elements sequence),
+  the ActorSystem's name, the hostname and the port."
+  [a]
+  `(cond (instance? String ~a) (AddressFromURIString/parse ~a)
+         (sequential? ~a) (condp = (count ~a)
+                           3 (Address. "akka" (nth ~a 0) (nth ~a 1) (nth ~a 2))
+                           4 (Address. (nth ~a 0) (nth ~a 1) (nth ~a 2) (nth ~a 3))
+                           (throw (IllegalArgumentException. "spawn:deploy-on should be either a String or a sequence of 3 or 4 elements")))
+         :else (throw (IllegalArgumentException. "spawn:deploy-on should be either a String or a sequence of 3 or 4 elements"))))
+
+(defn- with-deploy
+  [actor-spec address]
+  (if address
+    `(.withDeploy ~actor-spec (Deploy. (RemoteScope. ~(parse-address address))))
+    actor-spec))
+
+(defmacro spawn
+  "Spawns a new actor (side-effect) and returns an ActorRef to it. First
+  argument must be a Props object (such as created by the actor macro). Accepts
+  the following options:
+  :in designates the ActorSystem in which to create the ActorRef. If no :in option is given, the new actor is created in the context of the current one.
+  :router specifies a Router object to serve as a router for the returned ActorRef (see Akka documentation).
+  :name is used for both the full (logical) path of the returned ActorRef and for looking-up the relevant configuration concerning the to-be-created Actor (generated if none given).
+  :deploy-on must be the address of a remote ActorSystem in one of the three forms accepted by parse-address; the actor is remotely spawned on the remote system (as a root actor)."
+  [actor-spec & {c :in r :router n :name d :deploy-on
+                 :or {c '(.getContext this)}}]
+  (let [p (-> actor-spec
+            (with-router r)
+            (with-deploy d))]
     (if n `(.actorOf ~c ~p ~n)
       `(.actorOf ~c ~p))))
 
-(defn look-up [address & {s :in n :name}]
+(defn- string-to-vec
+  "Converts an address in string form to the corresponding vector form"
+  [a] (let [v (clojure.string/split a #"://|@|:|/")]
+        (conj (vec (take 4 v)) (drop 4 v))))
+
+(defn- vec-to-string
+  "Converts an address in vector form to its string representation"
+  [a] (let [a (vec a)]
+        (format "%s://%s@%s:%s/%s" (a 0) (a 1) (a 2) (a 3)
+                (clojure.string/join "/" (a 4)))))
+
+(defn- get-config-lookup
+  [config name]
+  (let [c (get-in config ["okku" "lookup" (str "/" name)])
+        extract (fn [k] (if-let [v (get c k)] (.unwrapped v)))]
+    [(extract "protocol") (extract "actor-system")
+     (extract "hostname") (extract "port")
+     (if-let [ps (extract "path")]
+       (let [p (clojure.string/split ps #"/")]
+         (if (= (first p) "")
+           (rest p)
+           (cons "user" p))))]))
+
+(defn- merge-addresses
+  "merges two addresses given in vector form [prot sys hn port & path]"
+  [a1 a2] (map #(if % % %2) a1 a2))
+
+(defn look-up
+  "Returns an ActorRef for the specified actor path.
+  :in specifies the ActorSystem in which to create the ActorRef
+  :name is a local name for the actor, used to allow for file-based
+  configuration of the look-up path."
+  [address & {s :in n :name}]
   (if-not s (throw (IllegalArgumentException. "okku.core/look-up needs an :in argument")))
-  (let [[prot sys hn port & path] (clojure.string/split address #"://|@|:|/")
-        {cprot "protocol" csys "actor-system" chn "hostname" cport "port"
-         cpath "path"} (get-in (.root (.. s settings config)) ["okku" "lookup" (str "/" n)])
-        [cprot csys chn cport cpath] (map #(if % (.unwrapped %)) [cprot csys chn cport cpath])
-        path (clojure.string/join "/" path)
-        cpath (if cpath (if (= (first path) \/) cpath (str "user/" cpath)))
-        address (str (or cprot prot) "://"
-                     (or csys sys) "@"
-                     (or chn hn) ":"
-                     (or cport port) "/"
-                     (or cpath path))]
+  (let [code-address-v (string-to-vec address)
+        config-address-v (get-config-lookup (.. s settings config root) n)
+        address-v (merge-addresses config-address-v code-address-v)
+        address (vec-to-string address-v)]
     (.actorFor s address)))
 
 (defmacro stop []
--- test/okku/test/core.clj
+++ test/okku/test/core.clj
@@ -2,5 +2,101 @@
   (:use [okku.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-!
+  (are [x y] (= (macroexpand-1 (quote x)) y)
+       (okku.core/! msg) '(.tell (.getSender this) msg (.getSelf this))
+       (okku.core/! target msg) '(.tell target msg (.getSelf this))))
+
+(deftest test-spawn
+  (are [x y] (= (macroexpand-1 (quote x)) y)
+       (okku.core/spawn act) '(.actorOf (.getContext this) act)
+       (okku.core/spawn act :in asys :router router :name name)
+       '(.actorOf asys (.withRouter act router) name)
+       (okku.core/spawn act :deploy-on addr)
+       '(.actorOf
+          (.getContext this)
+          (.withDeploy act
+                       (akka.actor.Deploy.
+                         (akka.remote.RemoteScope.
+                           (clojure.core/cond
+                             (clojure.core/instance? java.lang.String addr) (akka.actor.AddressFromURIString/parse
+                                                                              addr)
+                             (clojure.core/sequential? addr) (clojure.core/condp
+                                                               clojure.core/=
+                                                               (clojure.core/count addr)
+                                                               3
+                                                               (akka.actor.Address.
+                                                                 "akka"
+                                                                 (clojure.core/nth addr 0)
+                                                                 (clojure.core/nth addr 1)
+                                                                 (clojure.core/nth
+                                                                   addr
+                                                                   2))
+                                                               4
+                                                               (akka.actor.Address.
+                                                                 (clojure.core/nth addr 0)
+                                                                 (clojure.core/nth addr 1)
+                                                                 (clojure.core/nth addr 2)
+                                                                 (clojure.core/nth
+                                                                   addr
+                                                                   3))
+                                                               (throw
+                                                                 (java.lang.IllegalArgumentException.
+                                                                   "spawn:deploy-on should be either a String or a sequence of 3 or 4 elements")))
+                             :else (throw
+                                     (java.lang.IllegalArgumentException.
+                                       "spawn:deploy-on should be either a String or a sequence of 3 or 4 elements")))))))
+       ))
+
+(deftest test-dispatch-on
+  (are [x y] (= (macroexpand-1 x) y)
+       '(okku.core/dispatch-on t
+                               :dv1 (answer1)
+                               :dv2 (answer2))
+       '(clojure.core/cond (clojure.core/= t :dv1) (answer1)
+              (clojure.core/= t :dv2) (answer2)
+              :else (.unhandled this t))))
+
+(deftest test-string-to-vec
+  (are [x y] (= x (@#'okku.core/string-to-vec y))
+       ["akka" "sys" "hostname" "port" ["path1" "path2"]]
+       "akka://sys@hostname:port/path1/path2"
+       ["akka" "CalculatorApplication" "127.0.0.1" "2552" ["user" "simpleCalculator"]]
+       "akka://CalculatorApplication@127.0.0.1:2552/user/simpleCalculator"))
+
+(deftest test-vec-to-string
+  (are [x y] (= y (@#'okku.core/vec-to-string x))
+       ["akka" "sys" "hostname" "port" ["path1" "path2"]]
+       "akka://sys@hostname:port/path1/path2"
+       ["akka" "CalculatorApplication" "127.0.0.1" "2552" ["user" "simpleCalculator"]]
+       "akka://CalculatorApplication@127.0.0.1:2552/user/simpleCalculator"))
+
+(deftest test-get-config-lookup
+  (are [x y] (= x (@#'okku.core/get-config-lookup
+                       (..
+                         (com.typesafe.config.ConfigFactory/parseString y)
+                         root)
+                       "name"))
+       ["akka" "sys" "hostname" "port" ["path1" "path2"]]
+       "okku.lookup./name {
+       protocol = akka
+       actor-system = sys
+       hostname = hostname
+       port = port
+       path = /path1/path2
+       }
+       "
+       ["akka" nil nil "port" ["user" "path1" "path2"]]
+       "okku.lookup./name {
+       protocol = akka
+       port = port
+       path = path1/path2
+       }
+       "
+       [nil nil nil nil nil] ""))
+
+(deftest test-merge-addresses
+  (are [x y z] (= x (@#'okku.core/merge-addresses y z))
+       ["akka" "sys" "hn" "port" ["path1" "path2"]]
+       ["akka" nil nil "port" nil]
+       ["other" "sys" "hn" "other" ["path1" "path2"]]))
